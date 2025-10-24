20cbff1ed656667c7a1769813cb00c9f461d17e0
Kepler11145123 <yeisonhincapie13@gmail.com>
2025-10-24 17:41:57 -0500

    Fix python package imports with __init__.py

---
+++ models/entities/producto.py
@@ -0,0 +1,9 @@
+class Producto:
+    def __init__(self, id, nombre, descripcion, categoria, imagen_url, precio, stock):
+        self.id = id
+        self.nombre = nombre
+        self.descripcion = descripcion
+        self.categoria = categoria
+        self.imagen_url = imagen_url
+        self.precio = precio
+        self.stock = stock
