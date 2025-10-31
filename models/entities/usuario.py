5bb6e253199be583a9e1f062e0e2a1cf4fe644d6
Kepler11145123 <yeisonhincapie13@gmail.com>
2025-10-31 12:48:53 -0500

    a

--- models/entities/usuario.py
+++ models/entities/usuario.py
@@ -18,22 +18,3 @@ class Cliente(Usuario):
 class Administrador(Usuario):
     def __init__(self, id, nombre, correo, password):
         super().__init__(id, nombre, correo, password, 'administrador')
-
-
-# class Usuario(UserMixin):
-#     def __init__(self, id_usuario, nombre, correo, contraseña):
-#         self.id = id_usuario
-#         self.__nombre = nombre
-#         self.__correo = correo
-#         self.__contraseña = contraseña
-
-#     def get_correo(self):
-#         return self.__correo
-#     def get_contraseña(self):
-#         return self.__contraseña
-#     def get_nombre(self):
-#         return self.__nombre
-    
-#     # Método para verificar contraseña
-#     def verificar_contraseña(self, contraseña):
-#         return self.__contraseña == contraseña 
